21e8cf1eb9200f53520fedc6c9b005148bab4573
Joey Ashcroft <ashcroft.joey@gmail.com>
2019-05-31 21:14:22 -0700

    another heroku try

--- PGA_tour_viz/app.py
+++ PGA_tour_viz/app.py
@@ -15,8 +15,8 @@ app = Flask(__name__)
 # db = SQLAlchemy(app)
 
 #ATTEMPT FOR HEROKU DEPLOYMENT#########################
-app.config['MONGODB_URL'] = os.environ.get('mongodb://localhost:27017', '')
-client = pymongo.MongoClient(app)
+MONGODB_URL = os.environ.get('MONGODB_URL')
+client = pymongo.MongoClient(MONGODB_URL)
 db = client.pga_data['collection5']
 # MONGODB_URL = os.environ.get('MONGODB_URL')
 
